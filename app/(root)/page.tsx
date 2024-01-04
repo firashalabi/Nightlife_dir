956ecc7d41fb880c605d1e3ddab8b8a3aaeb301c
firas halabi <81621892+firashalabi@users.noreply.github.com>
2024-01-04 19:32:10 +0200

    finalization

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,21 @@
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 
 import Image from "next/image";
 import Link from "next/link";
 
 
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
   const events = await getAllEvents({
-    query: '',
+    query: searchText,
     category: '',
     page: 1,
     limit: 6
@@ -38,7 +44,7 @@ export default async function Home() {
     <section id="events" className="wrapper my-8 flex fex-col gap-8 md:gap-12">
       <h2 className="h2-bold">Trusted by <br /> Thousands of Events</h2>
       <div className="flex w-full flex-col gap-5 md:flex-row">
-        Search
+        <Search />
         CategoryFilter
       </div>
 
@@ -48,8 +54,8 @@ export default async function Home() {
       emptyStateSubtext="Come back later"
       collectionType="All_Events"
       limit={6}
-      page={1}
-      totalPages={2}
+      page={page}
+      totalPages={events?.totalPages}
       />
     </section>
    </>
